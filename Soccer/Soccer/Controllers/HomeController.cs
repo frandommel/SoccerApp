9f5e85afaacea5eeb338f422f2f39bc7c2faa43a
Francisco <pancho.dommel@gmail.com>
2020-07-31 13:46:11 -0300

    Termino funcionalidades del proyecto, tales como paginacion, y busqueda

--- Soccer/Soccer/Controllers/HomeController.cs
+++ Soccer/Soccer/Controllers/HomeController.cs
@@ -8,33 +8,71 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using DocumentFormat.OpenXml.InkML;
 using Newtonsoft.Json.Linq;
+using Soccer.Models.Paginador;
+using System;
 
 namespace Soccer.Controllers
 {
     public class HomeController : Controller
     {
+        private List<League> listLeague = new List<League>();
+        private Paginador<League> paginadorLeague;
         private readonly ILogger<HomeController> _logger;
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-
-        public IActionResult Index()
+        public IActionResult Index(string buscar,int pagina = 1)
         {
-            List<League> listLeague = new List<League>();
+            int totalRegistros = 0;
+            int registroPagina = 60;
             DataList listDataModel = new DataList{ listLeagueData = new List<League>() };
-
             listDataModel.listLeagueData = getData(listLeague);   //get data from json a build de model into DataList class
+            //logica para realizar la paginacion
+            totalRegistros = listDataModel.listLeagueData.Count;
+            var totalPaginas = (int)Math.Ceiling((double)totalRegistros / registroPagina);
+            if (!string.IsNullOrEmpty(buscar))
+            {
+                    for (int i = 0; i < totalRegistros; i++)
+                    {
+                         string buscado = listDataModel.listLeagueData[i].home_team.ToLower();
+                        if (buscado.Contains(buscar.Trim().ToLower()))
+                        {
+                            listLeague.Add(listDataModel.listLeagueData[i]);
+                        }
+                         registroPagina = listLeague.Count;
+                    }
+            } else {
+             
+                if (pagina - 1 == 0)
+                {
+                    for (int i = 0; i < registroPagina; i++)
+                    {
+                        listLeague.Add(listDataModel.listLeagueData[i]);
+                    }
+                }
+                for (int i = 0; i < (pagina - 1) * registroPagina; i++)
+                {
+                    listLeague.Add(listDataModel.listLeagueData[i]);
+                }
+            }
+            paginadorLeague = new Paginador<League>()
+            {
+                RegistroPorPagina = registroPagina,
+                TotalRegistros = totalRegistros,
+                TotalPaginas = totalPaginas,
+                PaginaActual = pagina,
+                Resultado = listLeague         
+            };
 
-            return View(listDataModel);
+            return View(paginadorLeague);
         }
 
         public List<League> getData(List<League> listLeague)
         {
             string fileJson = System.IO.File.ReadAllText(@"C:\Users\Francisco\source\repos\SoccerApp\Soccer\Soccer\Content\data.json");
             DataSet ds = (DataSet)JsonConvert.DeserializeObject(fileJson, typeof(DataSet));
-
             return ModelBuilder.ConvertFromDataTable<League>(ds.Tables[0]);
         }
 
---
+++ Soccer/Soccer/Models/Paginador/Paginador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Soccer.Models.Paginador
+{
+    public class Paginador<T> where T: class
+    {
+        public int PaginaActual { get; set; }
+        public int RegistroPorPagina { get; set; }
+        public int TotalRegistros { get; set; }
+        public int TotalPaginas { get; set; }
+        public IEnumerable<T> Resultado { get; set; }
+        public string BusquedaActual { get; set; }
+    }
+}
